354f265291d1b23967978bc7799f0a8565258f89
Logan Taehwan Lee <leelogan93@gmail.com>
2023-08-02 09:26:33 +0000

    Install tailwindcss/forms. Ref #3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };

0d5be6d0c2ef33e95bfdeebcb59c0dae36da9dfb
Logan Taehwan Lee <leelogan93@gmail.com>
2023-08-03 22:53:24 +0000

    Edit catalog.

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -25,10 +25,7 @@ export default function ProductCard({
           </div>
           <div className="mt-4 flex justify-between">
             <div>
-              <h3 className="text-sm text-gray-700">
-                <span aria-hidden="true" className="absolute inset-0" />
-                {product.name}
-              </h3>
+              <h3 className="text-sm text-gray-700">{product.name}</h3>
               <p className="mt-1 text-sm text-gray-500">
                 {product.description}
               </p>
--- client/src/pages/Catalog.tsx
+++ client/src/pages/Catalog.tsx
@@ -1,23 +1,9 @@
 import { Fragment, ReactElement, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { fetchCatalog, Product } from '../lib';
-import { Dialog, Menu, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Dialog, Transition } from '@headlessui/react';
 import ProductCard from '../components/ProductCard';
 
-const sortOptions = [
-  { name: 'Price: Low to High', href: '#', current: false },
-  { name: 'Price: High to Low', href: '#', current: false },
-];
-
-const subCategories = [
-  { name: 'Polos', href: '#' },
-  { name: 'Trousers', href: '#' },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
+const subCategories = [{ name: 'Polos' }, { name: 'Trousers' }];
 
 export default function Catalog(): ReactElement {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
@@ -85,9 +71,7 @@ export default function Catalog(): ReactElement {
                     <ul className="px-2 py-3 font-medium text-gray-900">
                       {subCategories.map((category) => (
                         <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
-                            {category.name}
-                          </a>
+                          <p className="block px-2 py-3">{category.name}</p>
                         </li>
                       ))}
                     </ul>
@@ -101,60 +85,13 @@ export default function Catalog(): ReactElement {
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-              New Arrivals
+              Men's Clothing
             </h1>
-
-            <div className="flex items-center">
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                    Sort
-                    <ChevronDownIcon
-                      className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95">
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      {sortOptions.map((option) => (
-                        <Menu.Item key={option.name}>
-                          {({ active }) => (
-                            <a
-                              href={option.href}
-                              className={classNames(
-                                option.current
-                                  ? 'font-medium text-gray-900'
-                                  : 'text-gray-500',
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm'
-                              )}>
-                              {option.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </div>
           </div>
-
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
             <h2 id="products-heading" className="sr-only">
               Products
             </h2>
-
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
               {/* Filters */}
               <form className="hidden lg:block">
@@ -162,7 +99,7 @@ export default function Catalog(): ReactElement {
                 <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
                   {subCategories.map((category) => (
                     <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
+                      <p>{category.name}</p>
                     </li>
                   ))}
                 </ul>

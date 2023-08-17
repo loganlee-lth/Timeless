05f67d8a1f4421517ea779d26f76cd8dd61d6a0f
Logan Taehwan Lee <leelogan93@gmail.com>
2023-08-17 04:11:33 +0000

    Add new footer.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,77 +1,81 @@
 import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
-type NavigationItem = {
+type IconProps = {
+  className?: string;
+  'aria-hidden'?: boolean;
+};
+
+type Item = {
   name: string;
   to: string;
+  icon: (props: IconProps) => ReactElement;
 };
 
 type FooterNavigation = {
-  shop: NavigationItem[];
-  connect: NavigationItem[];
+  social: Item[];
 };
 
 const footerNavigation: FooterNavigation = {
-  shop: [{ name: 'Products', to: '/catalog' }],
-  connect: [
-    { name: 'GitHub', to: 'https://github.com/loganlee-lth' },
-    { name: 'LinkedIn', to: 'https://www.linkedin.com/in/logan-lee-lth/' },
+  social: [
+    {
+      name: 'LinkedIn',
+      to: 'https://www.linkedin.com/in/logan-lee-lth/',
+      icon: (props: any) => (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-linkedin"
+          viewBox="0 0 16 16"
+          {...props}>
+          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+        </svg>
+      ),
+    },
+    {
+      name: 'GitHub',
+      to: 'https://github.com/loganlee-lth',
+      icon: (props: any) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
   ],
 };
 
 export default function Footer(): ReactElement {
   return (
-    <div className="bg-white">
-      <footer aria-labelledby="footer-heading" className="bg-gray-900">
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="py-20 xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-              <div className="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                <div>
-                  <h3 className="text-sm font-medium text-white">Shop</h3>
-                  <ul className="mt-6 space-y-6">
-                    {footerNavigation.shop.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <Link
-                          to={item.to}
-                          className="text-gray-300 hover:text-white">
-                          {item.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                <div>
-                  <h3 className="text-sm font-medium text-white">Connect</h3>
-                  <ul className="mt-6 space-y-6">
-                    {footerNavigation.connect.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <Link
-                          to={item.to}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-gray-300 hover:text-white">
-                          {item.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
+    <footer aria-labelledby="footer-heading" className="relative">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-4 lg:px-8">
+        <div className="border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
+          <div className="flex space-x-6 md:order-2">
+            {footerNavigation.social.map((item) => (
+              <Link
+                key={item.name}
+                to={item.to}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 hover:text-gray-400">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden={true} />
+              </Link>
+            ))}
           </div>
-          <div className="border-t border-gray-800 py-10">
-            <p className="text-sm text-gray-400">
-              Copyright &copy; 2023 Logan Lee
-            </p>
-          </div>
+          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+            Copyright &copy; 2023 Logan Lee. All Rights Reserved.
+          </p>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 }
